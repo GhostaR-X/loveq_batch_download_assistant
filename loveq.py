fef80fb9e637029f7769350e2e9184a5e9ec7265
ghostar <ghostar@163.com>
2016-10-12 16:02:43 +0800

    loveq batch download OK

--- loveq.py
+++ loveq.py
@@ -3,28 +3,69 @@
 # --------------
 
 import urllib.request
-from datetime import datetime 
+from datetime import datetime
+from datetime import timedelta
+import sys
 
+DayOfWeek = ('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday') 
+#Monday = 0,Tuesday = 1, ... Sunday = 6
 base_url = "http://dl.loveq.cn/program/"
 
-date = datetime(2016,10,1).strftime('%Y-%m-%d')
-print(date)
+if __name__ == '__main__':
 
-filename = date + "_LoveQ.mp3"
-print(filename)
+    args = sys.argv
+    if len(args) < 3:
+        print("Usage:loveq.py save_directory from_date(%Y-%m-%d) [to_date]")
+        exit()
 
-target_url = base_url + "LoveQ.cn_" + date + "-1.mp3"
-#print(target_url)
+    save_dir = args[1]
+    from_date_str = args[2]
+    if len(args) == 4:
+        to_date_str = args[3]
+    else:
+        to_date_str = from_date_str    
 
-print ("Downloading",target_url)
-urllib.request.urlretrieve(target_url, filename)
-print("Finish!")
+    from_date = datetime.strptime(from_date_str,'%Y-%m-%d')
+    #print(from_date)
+
+    if to_date_str != from_date_str:
+        to_date = datetime.strptime(to_date_str,'%Y-%m-%d')
+        #print(to_date)
+
+        while to_date != from_date : 
+            week = from_date.weekday()
+            if DayOfWeek[week] != 'Saturday' and DayOfWeek[week] != 'Sunday' :
+                #print('unavailable')
+                from_date = from_date + timedelta(days=1) 
+                continue
+            else :
+                #print(from_date)
+
+                date = from_date.strftime('%Y-%m-%d')
+                #print(date)
+                filename = save_dir + '/' + date + "-LoveQ.mp3"
+                #print(filename)
+                target_url = base_url + "LoveQ.cn_" + date + "-1.mp3"
+                print ("Downloading",target_url)
+                urllib.request.urlretrieve(target_url, filename)
+
+                from_date = from_date + timedelta(days=1) 
+        
+        print("Tasks Finish!")            
 
 
+    else:   #if to_date_str == from_date_str
+        week = from_date.weekday()
+        if DayOfWeek[week] != 'Saturday' and DayOfWeek[week] != 'Sunday' :
+            print("It is not an available date for download!")
+            exit() 
 
+        date = from_date.strftime('%Y-%m-%d')
+        #print(date)
+        filename = save_dir + '/' + date + "-LoveQ.mp3"
+        #print(filename)
+        target_url = base_url + "LoveQ.cn_" + date + "-1.mp3"
+        print ("Downloading",target_url)
+        urllib.request.urlretrieve(target_url, filename)
+        print("Finish!")
 
-#url = "http://dl.loveq.cn/program/LoveQ.cn_2016-10-08-1.mp3" 
-
-#print ("Downloading with urllib!")
-#urllib.request.urlretrieve(url, "demo.mp3")
-#print("Finish!")
